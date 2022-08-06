1c0c8d9037373a8ac3b14ee64a787c0ef466518a
Ansh Bhardwaj <93650767+anshyo@users.noreply.github.com>
2022-08-06 19:19:11 +0000

    Add files via upload

---
+++ assigment.py
@@ -0,0 +1,64 @@
+# question1
+
+# n= int(input("till where you wanna take your numbers: "))
+# a=0
+# while a!=n:
+#     a=a+1
+#     print(a, end=" ")
+# a=0
+# b=a   
+# while b!=n:
+#     b=b+1
+#     a=a+b
+# print("\nsum of all those numbers are",a)
+# print("average of all those numbers are",a/n)
+
+
+
+# question2
+
+# m=int(input("enter starting number: "))
+# n=int(input("enter ending number: "))
+# for i in range(m,n):
+#     if i<=0:
+#         pass
+#     else:
+#         print(i , end=" ")
+
+
+
+# question3
+
+# x= int(input("num: "))
+# n= int(input("enter till where you wanna write your table:"))
+# for m in range (1,n+1):
+#     print(x,"x",m,"=",x*m)
+
+
+
+# question4
+
+# y= int(input("enter ,till where you wanna write your odd numbers: "))
+# a=1
+# while a<=y:
+#     print(a , end=" ")
+#     a=a+2
+
+
+
+# question5
+
+n=int(input("enter, till where you wanna take your series: "))
+# a=1
+# b=0
+# while a<=n:
+#     b=b+1
+#     print(a, end=" ")
+#     a=a+b
+a=1
+b=0
+while a<=n:
+    b=b+1
+    a=a+b
+    a=a+a
+print(a)
