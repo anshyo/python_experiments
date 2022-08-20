e41349e8f05b9d4fc140828efae85570b37158bc
Ansh Bhardwaj <93650767+anshyo@users.noreply.github.com>
2022-08-20 18:20:46 +0530

    a

--- assig.py
+++ assig.py
@@ -97,5 +97,124 @@
 
 # question9
 
-n=list((input("write numberin this format([2,3,4,5,6,7,8]): ")))
-print("maximum number is" ,max(n))
+# lisT=[]
+# n=int(input("how many numbers should be in your list: "))
+# print("numns: ",end='')
+# for i in range(0,n):
+#     num=int(input())
+#     lisT.append(num)   
+# lis=lisT
+# print("maximum number is",lis)
+
+
+
+# question10
+
+# n=int(input("enter your number: "))
+# sum=0
+# while n!=0:
+#     dig=n%10
+#     sum=sum+dig
+#     n=n//10
+# print("sum of all digits in number is",sum)
+
+
+
+# question11
+
+# num= int(input("enter a number:"))
+# f=False
+# if num>1:
+#     for i in range(2,num):
+#         if num%i==0:
+#             f=True
+#             break
+# if f:
+#     print("numbers is not primenumber")
+# else:
+#     print("number is prime number")
+
+
+
+# question12
+
+# n=int(input("enter your number: "))
+# print("your reversed idgit is",end=" ")
+# while n!=0:
+#     dig=n%10
+#     print(dig,end='')
+#     n=n//10
+
+
+
+# question13
+
+# n=int(input("enter your number: "))
+# m=n
+# reverse=0
+# while n!=0:
+#     dig=n%10
+#     reverse= reverse*10+dig
+#     n=n//10
+# print(reverse)
+# if reverse==m:
+#     print("its a palindrome")
+# else:
+#     print("its not a palindrome")
+
+
+
+# question14
+
+# n=int(input("time where you wanna take your fabonacci table: "))
+# a=0
+# b=1
+# c=1
+# print(a,end=" ")
+# while c<=n:
+#     print(c, end=" ")
+#     c=a+b
+#     a=b
+#     b=c
+
+
+
+# question15
+
+# yet to do
+
+
+
+# question16
+
+# n= int(input("write a number(positive): "))
+# b=0
+# while b!=n:
+#     b=b+1
+#     print('* '*b)
+
+
+
+# question17
+
+# n= int(input("write a number(positive): "))
+# while n!=0:
+#     print('* '*n)
+#     n=n-1
+
+
+
+# question18
+
+# yet to be done
+
+
+
+# question19
+
+for j in range(1,101):
+    for i in range(2,j):
+        if j%i==0:
+            pass
+        else:
+            print(i)
