4a16dbd41ca0a148d0e995be6d36f4e762735e1c
The Ansh <ansh.bhardwaj457@gmail.com>
2023-01-04 18:41:25 +0530

    a

--- mutable A.I/main.py
+++ mutable A.I/main.py
@@ -1,4 +1,8 @@
 import _func
 
 if __name__ == '__main__' :
-    _func.run()
+    a = input('What do you wanna do?( reset("r") / run normally("n") ) / to change your name("c") : ').lower()
+    if a == 'r':
+        _func.reset()
+    elif a == 'n':
+        _func.run()
